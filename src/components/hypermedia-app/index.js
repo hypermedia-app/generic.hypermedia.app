5d39f1cec4e8da6f463547ee06823451051d6626
tpluscode <tpluscode@users.noreply.github.com>
2019-01-07 13:15:48 +0100

    add welcoming text and loader logo

--- src/components/hypermedia-app/index.js
+++ src/components/hypermedia-app/index.js
@@ -19,6 +19,7 @@ let HypermediaApp = class HypermediaApp extends PolymerElement {
         import('@hydrofoil/hydrofoil-paper-shell/hydrofoil-address-bar');
         import('../entrypoint-selector');
         import('../../views');
+        import('@polymer/iron-icon/iron-icon');
     }
     navigate() {
         fireNavigation(this, this.address.url);
@@ -28,6 +29,10 @@ let HypermediaApp = class HypermediaApp extends PolymerElement {
     }
     static get template() {
         return html `
+      <style>
+        div { padding: 20px }
+      </style>
+
       <hydrofoil-paper-shell url="{{url}}" use-hash-urls>
         <app-toolbar slot="toolbar-left">
           <entrypoint-selector main-title on-url-changed="updateAddressBar">
@@ -39,6 +44,25 @@ let HypermediaApp = class HypermediaApp extends PolymerElement {
         <app-toolbar slot="header">
           <hydrofoil-address-bar main-title url="[[url]]" on-resource-confirmed="navigate"></hydrofoil-address-bar>
         </app-toolbar>
+
+        <div slot="shell-ready">
+          Hi,
+
+          This is the Hydra Console - a generic client for Hydra-powered Web APIs. The
+          user interface consists of four parts:
+
+          <ol>
+            <li>the address bar on the top,</li>
+            <li>the main resource area, which renders response contents,</li>
+            <li>entrypoint links in left sidebar,</li>
+            <li>documentation pane in right sidebar (coming soon).</li>
+          </ol>
+
+          You may also start by selecting an API from the dropdown from the sidebar header.
+
+          Links are presented as a clickable icon <iron-icon icon="link"></iron-icon>. Clicking them will fetch the
+          resource and present it in the resource area.
+        </div>
       </hydrofoil-paper-shell>`;
     }
 };
--- src/components/hypermedia-app/index.ts
+++ src/components/hypermedia-app/index.ts
@@ -28,6 +28,7 @@ export default class HypermediaApp extends PolymerElement {
     import('@hydrofoil/hydrofoil-paper-shell/hydrofoil-address-bar')
     import('../entrypoint-selector')
     import('../../views')
+    import('@polymer/iron-icon/iron-icon')
   }
 
   private navigate() {
@@ -40,6 +41,10 @@ export default class HypermediaApp extends PolymerElement {
 
   static get template() {
     return html`
+      <style>
+        div { padding: 20px }
+      </style>
+
       <hydrofoil-paper-shell url="{{url}}" use-hash-urls>
         <app-toolbar slot="toolbar-left">
           <entrypoint-selector main-title on-url-changed="updateAddressBar">
@@ -51,6 +56,25 @@ export default class HypermediaApp extends PolymerElement {
         <app-toolbar slot="header">
           <hydrofoil-address-bar main-title url="[[url]]" on-resource-confirmed="navigate"></hydrofoil-address-bar>
         </app-toolbar>
+
+        <div slot="shell-ready">
+          Hi,
+
+          This is the Hydra Console - a generic client for Hydra-powered Web APIs. The
+          user interface consists of four parts:
+
+          <ol>
+            <li>the address bar on the top,</li>
+            <li>the main resource area, which renders response contents,</li>
+            <li>entrypoint links in left sidebar,</li>
+            <li>documentation pane in right sidebar (coming soon).</li>
+          </ol>
+
+          You may also start by selecting an API from the dropdown from the sidebar header.
+
+          Links are presented as a clickable icon <iron-icon icon="link"></iron-icon>. Clicking them will fetch the
+          resource and present it in the resource area.
+        </div>
       </hydrofoil-paper-shell>`
   }
 }
