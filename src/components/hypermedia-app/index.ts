2669d1d68b973bbcb9f26e0eae2e85793e5c5977
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2019-05-01 13:31:51 +0200

    Update index.ts

--- src/components/hypermedia-app/index.ts
+++ src/components/hypermedia-app/index.ts
@@ -97,7 +97,7 @@ export default class HypermediaApp extends PolymerElement {
         <app-toolbar slot="drawer-left">
           <entrypoint-selector main-title on-url-changed="updateAddressBar">
             <span data-url="https://hydra-movies.herokuapp.com/">HydraCG movies API</span>
-            <span data-url="https://test.wikibus.org/">wikibus.org sources (test server)</span>
+            <span data-url="https://sources.test.wikibus.org/">wikibus.org sources (test server)</span>
           </entrypoint-selector>
         </app-toolbar>
 
