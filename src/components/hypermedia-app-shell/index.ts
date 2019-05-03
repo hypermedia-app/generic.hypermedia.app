ca9bb9678e8b687a445cf3a5709fa65047fcefb2
tpluscode <tpluscode@users.noreply.github.com>
2019-05-03 09:42:45 +0200

    the spinner was not being imported

--- src/components/hypermedia-app-shell/index.ts
+++ src/components/hypermedia-app-shell/index.ts
@@ -1,6 +1,7 @@
 import AlcaeusLoader from '@hydrofoil/alcaeus-loader'
 import {HydrofoilPaperShell} from '@hydrofoil/hydrofoil-paper-shell/hydrofoil-paper-shell'
 import '@hydrofoil/hydrofoil-paper-shell/hydrofoil-resource-accordion'
+import '@polymer/paper-spinner/paper-spinner'
 import {customElement} from 'lit-element'
 
 @customElement('hypermedia-app-shell')
