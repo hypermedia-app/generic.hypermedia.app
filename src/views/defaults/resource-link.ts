afaea712103985bb52367570fa4e27c6b3b9720e
tpluscode <tpluscode@users.noreply.github.com>
2019-05-23 22:11:49 +0200

    use new property

--- src/views/defaults/resource-link.ts
+++ src/views/defaults/resource-link.ts
@@ -1,14 +1,15 @@
 import ViewTemplates from '@lit-any/lit-any/views'
+import {IResource} from 'alcaeus/types/Resources/Resource'
 import {html} from 'lit-html'
 import {getPath} from '../helpers'
 
 ViewTemplates.default.when
-  .valueMatches((v) => !!v['@id'] && !v['@id'].match(/^_/)) // TODO: simpler way to tell a blank node
-  .renders((v, r, scope, params) => {
+  .valueMatches((v: IResource) => v && !v.isAnonymous)
+  .renders((v: IResource, r, scope, params) => {
     import('@polymer/iron-icon/iron-icon')
 
     if (params.isLink) {
-      return html`<ld-link resource-url="${v['@id']}">
+      return html`<ld-link resource-url="${v.id}">
 <a><iron-icon icon=link></iron-icon></a>
 </ld-link>`
     }
