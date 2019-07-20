20b40a36d68e48c6a70efabcbe5607dea3e8f0f1
tpluscode <tpluscode@users.noreply.github.com>
2019-07-20 12:34:07 +0200

    fix: follow location header on 201 responses

--- src/components/hypermedia-app-shell/index.ts
+++ src/components/hypermedia-app-shell/index.ts
@@ -2,6 +2,7 @@ import AlcaeusLoader from '@hydrofoil/alcaeus-loader'
 import { HydrofoilPaperShell } from '@hydrofoil/hydrofoil-paper-shell/hydrofoil-paper-shell'
 import '@hydrofoil/hydrofoil-paper-shell/hydrofoil-resource-accordion'
 import '@polymer/paper-spinner/paper-spinner'
+import { Hydra } from 'alcaeus'
 import { HydraInvokeOperationEvent } from '../../forms'
 
 export default class HypermediaAppShell extends AlcaeusLoader(HydrofoilPaperShell) {
@@ -22,6 +23,10 @@ export default class HypermediaAppShell extends AlcaeusLoader(HydrofoilPaperShel
       response = await e.detail.operation.invoke(body)
     } else {
       response = await e.detail.operation.invoke(null)
+    }
+
+    if (response.xhr.status === 201 && response.xhr.headers.has('location')) {
+      response = await Hydra.loadResource(response.xhr.headers.get('Location'))
     }
 
     this.model = response.root
